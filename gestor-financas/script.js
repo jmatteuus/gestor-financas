c208885992fe35dc06232b36bb5c199f23aa701f
João Mateus <joaomateuscvieira@gmail.com>
2024-10-12 13:29:58 -0300

    second commit

--- gestor-financas/script.js
+++ gestor-financas/script.js
@@ -3,25 +3,25 @@ window.onload = function recuperar(){
     if(atualcor){
         atual.style.color = atualcor
     }
-    let faculdadetxt = localStorage.getItem('faculdadetxt')
-    if(faculdadetxt){
-        document.getElementById('faculdade').text = faculdadetxt
+    let gasto1txt = localStorage.getItem('gasto1txt')
+    if(gasto1txt){
+        document.getElementById('gasto1').text = gasto1txt
     }
-    let passagemtxt = localStorage.getItem('passagemtxt')
-    if(passagemtxt){
-        document.getElementById('passagem').text = passagemtxt
+    let gasto2txt = localStorage.getItem('gasto2txt')
+    if(gasto2txt){
+        document.getElementById('gasto2').text = gasto2txt
     }
-    let oculostxt = localStorage.getItem('oculostxt')
-    if(oculostxt){
-        document.getElementById('oculos').text = oculostxt
+    let gasto3txt = localStorage.getItem('gasto3txt')
+    if(gasto3txt){
+        document.getElementById('gasto3').text = gasto3txt
     }
-    let guardartxt = localStorage.getItem('guardartxt')
-    if(guardartxt){
-        document.getElementById('guardar').text = guardartxt
+    let gasto4txt = localStorage.getItem('gasto4txt')
+    if(gasto4txt){
+        document.getElementById('gasto4').text = gasto4txt
     }
-    let donativotxt = localStorage.getItem('donativotxt') 
-    if(donativotxt){
-        document.getElementById('donativo').text = donativotxt
+    let gasto5txt = localStorage.getItem('gasto5txt') 
+    if(gasto5txt){
+        document.getElementById('gasto5').text = gasto5txt
     }
 }
 
@@ -64,37 +64,37 @@ function colocar(){
 function resetar(){
     localStorage.removeItem('salario')
     localStorage.removeItem('atualcor')
-    localStorage.removeItem('faculdadetxt')
-    localStorage.removeItem('passagemtxt')
-    localStorage.removeItem('oculostxt')
-    localStorage.removeItem('guardartxt')
-    localStorage.removeItem('donativotxt')
+    localStorage.removeItem('gasto1txt')
+    localStorage.removeItem('gasto2txt')
+    localStorage.removeItem('gasto3txt')
+    localStorage.removeItem('gasto4txt')
+    localStorage.removeItem('gasto5txt')
     salario = 1500
     atual.innerHTML = `Dinheiro atual R$${salario.toFixed(2)}`
     atual.style.color = 'green'
     resultado.innerHTML = ''
-    document.getElementById('faculdade').text = 'Faculdade (R$150,00) Clique e pague!'
-    document.getElementById('passagem').text = 'Passagem libras (R$150,00) Clique e pague!'
-    document.getElementById('oculos').text = 'Óculos (R$50,00) Clique e pague!'
-    document.getElementById('guardar').text = 'Guardar (R$600,00) Clique e pague!'
-    document.getElementById('donativo').text = 'Donativo (R$50,00) Clique e pague!'
+    document.getElementById('gasto1').text = 'Gasto um (R$150,00) Clique e pague!'
+    document.getElementById('gasto2').text = 'Gasto dois (R$150,00) Clique e pague!'
+    document.getElementById('gasto3').text = 'Gasto três (R$50,00) Clique e pague!'
+    document.getElementById('gasto4').text = 'Gasto quatro (R$600,00) Clique e pague!'
+    document.getElementById('gasto5').text = 'Gasto cinco (R$50,00) Clique e pague!'
 }
 
-function faculdade(){
+function gasto1(){
 
-    let valorfaculdade = 150
+    let valorgasto1 = 150
 
-    if (salario <= 0 || valorfaculdade > salario){
+    if (salario <= 0 || valorgasto1 > salario){
         alert('Você não tem dinheiro suficiente!')
         return
     }
 
-    salario-= valorfaculdade
+    salario-= valorgasto1
     localStorage.setItem('salario', salario)
     atual.innerHTML = `Dinheiro atual R$${salario.toFixed(2)}`
-    let flista = document.getElementById('faculdade')
-    flista.text = `Faculdade (R$150,00) PAGO\u{1F973}`
-    localStorage.setItem('faculdadetxt', flista.text)
+    let flista = document.getElementById('gasto1')
+    flista.text = `Gasto um (R$150,00) PAGO\u{1F973}`
+    localStorage.setItem('gasto1txt', flista.text)
     if (salario <= 400){
         atual.style.color = 'red'
         localStorage.setItem('atualcor', atual.style.color)
@@ -104,21 +104,21 @@ function faculdade(){
      }
 }
 
-function passagem(){
+function gasto2(){
 
-    let valorpassagem = 150
+    let valorgasto2 = 150
 
-    if (salario <= 0 || valorpassagem > salario){
+    if (salario <= 0 || valorgasto2 > salario){
         alert('Você não tem dinheiro suficiente!')
         return
     }
 
-    salario-= valorpassagem
+    salario-= valorgasto2
     localStorage.setItem('salario', salario)
     atual.innerHTML = `Dinheiro atual R$${salario.toFixed(2)}`
-    let llista = document.getElementById('passagem')
-    llista.text = `Passagem libras (R$150,00) PAGO\u{1F973}`
-    localStorage.setItem('passagemtxt', llista.text)
+    let llista = document.getElementById('gasto2')
+    llista.text = `Gasto dois (R$150,00) PAGO\u{1F973}`
+    localStorage.setItem('gasto2txt', llista.text)
     if (salario <= 400){
         atual.style.color = 'red'
         localStorage.setItem('atualcor', atual.style.color)
@@ -128,21 +128,21 @@ function passagem(){
      }
 }
 
-function oculos(){
+function gasto3(){
 
-    let valoroculos = 50
+    let valorgasto3 = 50
 
-    if (salario <= 0 || valoroculos > salario){
+    if (salario <= 0 || valorgasto3 > salario){
         alert('Você não tem dinheiro suficiente!')
         return
     }
 
-    salario-= valoroculos
+    salario-= valorgasto3
     localStorage.setItem('salario', salario)
     atual.innerHTML = `Dinheiro atual R$${salario.toFixed(2)}`
-    let olista = document.getElementById('oculos')
-    olista.text = `Óculos (R$50,00) PAGO\u{1F973}`
-    localStorage.setItem('oculostxt', olista.text)
+    let olista = document.getElementById('gasto3')
+    olista.text = `Gasto três (R$50,00) PAGO\u{1F973}`
+    localStorage.setItem('gasto3txt', olista.text)
     if (salario <= 400){
         atual.style.color = 'red'
         localStorage.setItem('atualcor', atual.style.color)
@@ -151,21 +151,21 @@ function oculos(){
         localStorage.setItem('atualcor', atual.style.color)
      }
 }
-function guardar(){
+function gasto4(){
 
-    let valorguardar = 600
+    let valorgasto4 = 600
 
-    if (salario <= 0 || valorguardar > salario){
+    if (salario <= 0 || valorgasto4 > salario){
         alert('Você não tem dinheiro suficiente!')
         return
     }
 
-    salario -= valorguardar
+    salario -= valorgasto4
     localStorage.setItem('salario', salario)
     atual.innerHTML = `Dinheiro atual R$${salario.toFixed(2)}`
-    let glista = document.getElementById('guardar')
-    glista.text = `Guardar (R$600,00) PAGO\u{1F973}`
-    localStorage.setItem('guardartxt', glista.text)
+    let glista = document.getElementById('gasto4')
+    glista.text = `Gasto quatro (R$600,00) PAGO\u{1F973}`
+    localStorage.setItem('gasto4txt', glista.text)
     if (salario <= 400){
         atual.style.color = 'red'
         localStorage.setItem('atualcor', atual.style.color)
@@ -174,21 +174,21 @@ function guardar(){
         localStorage.setItem('atualcor', atual.style.color)
      }
 }
-function donativo(){
+function gasto5(){
 
-    let valordonativo = 50
+    let valorgasto5 = 50
 
-    if (salario <= 0 || valordonativo > salario){
+    if (salario <= 0 || valorgasto5 > salario){
         alert('Você não tem dinheiro suficiente!')
         return
     }
 
-    salario -= valordonativo
+    salario -= valorgasto5
     localStorage.setItem('salario', salario)
     atual.innerHTML = `Dinheiro atual R$${salario.toFixed(2)}`
-    let dlista = document.getElementById('donativo')
-    dlista.text = `Donativo (R$50,00) PAGO\u{1F973}`
-    localStorage.setItem('donativotxt', dlista.text)
+    let dlista = document.getElementById('gasto5')
+    dlista.text = `Gasto cinco (R$50,00) PAGO\u{1F973}`
+    localStorage.setItem('gasto5txt', dlista.text)
     if (salario <= 400){
         atual.style.color = 'red'
         localStorage.setItem('atualcor', atual.style.color)
